36144c04364c55c20910cc5cf91df86d5cc0697c
Chineze-prog <chinezeobi.bomi@gmail.com>
2024-09-13 19:10:27 -0500

    implementing calculator

---
+++ calculator.py
@@ -0,0 +1,16 @@
+import tkinter 
+
+calculator = ""
+
+def addToCalculation(symbol):
+    pass
+
+def evaluateCalculation():
+    pass
+
+def clear():
+    pass
+
+root = tkinter.Tk()
+root.geometry()
+root.mainloop()
