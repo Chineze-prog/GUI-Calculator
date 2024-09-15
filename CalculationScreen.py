e89c3a22b8d56bc03e63ac3c5836d2ea6b0fdc2c
Chineze-prog <chinezeobi.bomi@gmail.com>
2024-09-15 00:15:42 -0500

    implementing calculation function

--- CalculationScreen.py
+++ CalculationScreen.py
@@ -7,10 +7,11 @@ class CalculatorScreen(tk.Message):
        
     def equate(self):
         try:
-            answer = eval(self.cget("text").replace("×", "*").replace("÷", "/").replace("%", "*(1/100)*"))
+            answer = eval(self.cget("text").replace("×", "*").replace("÷", "/"))
             self["text"] = answer
             self["font"] = ("Arial", 40)
             self.hasEquated = True
+            
         except:
             self["text"] = "Error"
     
@@ -27,6 +28,18 @@ class CalculatorScreen(tk.Message):
         if "Error" not in equation:
             if equation == "0": 
                 self["text"] = text
+            elif text == "%":
+                self.hasEquated = True
+                # find the last number in the equation
+                i = len(equation) - 1
+                while i >= 0 and (equation[i].isdigit() or equation[i] == "."):
+                    i -= 1
+                lastNumber = float(equation[i+1:])
+                percentage = lastNumber * 0.01
+                if percentage < 0.01: # Show at least 3 significant digits
+                    self["text"] = f"{equation[:i+1]}{percentage:.3g}"
+                else: # Show 3 digits after decimal
+                    self["text"] = f"{equation[:i+1]}{percentage:.3f}"
             elif text in operators and equation[-1:] in operators:
                 self["text"] = f"{equation[:-1]}{text}"
             else:
@@ -34,13 +47,12 @@ class CalculatorScreen(tk.Message):
         else:
             self.clearAll()
             self["text"] = text
-            
     
     def reverseSign(self):
-        self["text"] = -int(self.cget("text"))
+        self["text"] = -float(self.cget("text"))
     
     def reduceFont(self):
-        self["font"] = ("Arial", int(self.cget("font")[-2:]) - 3)
+        self["font"] = ("Arial", int(self.cget("font")[-2:]) - 6)
        
     def clearPrev(self): 
         equation = self.cget("text")
